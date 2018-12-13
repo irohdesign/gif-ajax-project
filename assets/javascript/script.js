7c5ec60a15787a11d3bcedc7915b5fa050e44d25
Jalia Evans <irohdesign@outlook.com>
2018-12-12 21:00:36 -0500

    now able to add new buttons to the topics array from the user's text input

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,24 +5,33 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword + "&api_key
 // array of strings for topics
 var topics = ["history", "epidemics", "parks and recreation", "the office", "greys anatomy", "simplynailogical"];
 
-for (var i = 0; i < topics.length; i++) {
-    // need to empty div so we don't get repeat buttons on every click
 
-    // create button for each topic
-    var topicBtn = $("<button>");
-    $(topicBtn).text(topics[i]);
-    $("#button-container").append(topicBtn);
-}
 
-// $(document).on("click", function () {
-//     // $.ajax({
-//     //     url: queryURL,
-//     //     method: "GET"
-//     // }).then(function(response) {
-//     //     console.log(response);
-//     // })
 
-//     //loop to create buttons for topics array
+$("#btn").on("click", function () {
+    event.preventDefault();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+
+        var userInput = $("#testInput").val();
+        topics.push(userInput);
+
+
+        for (var i = 0; i < topics.length; i++) {
+            // need to empty div so we don't get repeat buttons on every click
+
+            // create button for each topic
+            var topicBtn = $("<button>");
+            $(topicBtn).text(topics[i]);
+            $("#button-container").append(topicBtn);
+        }
+    })
+
+    //loop to create buttons for topics array
     
     
-// })
+})
