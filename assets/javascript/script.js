f526fcd89e997fe4a03399a319db158c07fa8d6c
Jalia Evans <irohdesign@outlook.com>
2018-12-12 22:00:26 -0500

    can successfully play and pause gifs by changing different attributes with an if statement

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,25 @@
 // api key = IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx
-var searchword = "Ryan Gosling";
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword + "&api_key=IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx&limit=5"
 
 // array of strings for topics
-var topics = ["history", "epidemics", "parks and recreation", "the office", "greys anatomy", "simplynailogical"];
+var topics = ["history", "sick", "happy", "the office", "greys anatomy", "jennamarbles"];
+
+// var searchTerm = "cats";
+
+for (var i = 0; i < topics.length; i++) {
+
+    // create button for each topic
+    var topicBtn = $("<button>");
+    $(topicBtn).text(topics[i]).attr("class", "btn").attr("data-value", topics[i]);
+    $("#button-container").append(topicBtn);
+}
 
 
-$("#btn").on("click", function () {
+$(".btn").on("click", function () {
     event.preventDefault();
+
+    var searchTerm = $(this).attr("data-value");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx&limit=10"
+
 
     $.ajax({
         url: queryURL,
@@ -15,31 +27,34 @@ $("#btn").on("click", function () {
     }).then(function(response) {
         console.log(response);
 
-        // var userInput = $("#testInput").val();
+        var userInput = $("#testInput").val();
         var results = response.data;
+
         console.log(results);
+        topics.push(userInput);
 
-        // topics.push(userInput);
-
-        // for (var i = 0; i < topics.length; i++) {
-        //     // need to empty div so we don't get repeat buttons on every click
-
-        //     // create button for each topic
-        //     var topicBtn = $("<button>");
-        //     $(topicBtn).text(topics[i]);
-        //     $("#button-container").append(topicBtn);
-        // }
-
-        // grab 10 static gif images and display them
+        // // grab 10 static gif images and display them
         for (var i = 0; i < results.length; i++) {
             var staticImgURL = results[i].images.original_still.url;
+            var animatedURL = results[i].images.original.url;
+
             var imgContainer = $("<img>");
-            imgContainer.attr("src", staticImgURL);
-            $("#button-container").append(imgContainer);
-            // console.log(results[i].type);
+            imgContainer.attr("src", staticImgURL).attr("state", "still").attr("class", "genImage").attr("animate", animatedURL).attr("stopped", staticImgURL);
+            $("#images-container").append(imgContainer);
         }
 
+        $(".genImage").on("click", function() {
         
+                    if ($(this).attr("state") === "still") {
+                        var grabAnimated = $(this).attr("animate");
+                        $(this).attr("src", grabAnimated);
+                        $(this).attr("state", "animated");
+                        console.log("it's stopped");
+                    } else {
+                        var grabStatic = $(this).attr("stopped");
+                        $(this).attr("src", grabStatic);
+                    }
+                })
 
     })
 
@@ -47,3 +62,4 @@ $("#btn").on("click", function () {
     
     
 })
+
