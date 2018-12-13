a57f41b876223e693a1bfc10ecbc35cf36744f34
Jalia Evans <irohdesign@outlook.com>
2018-12-12 20:54:58 -0500

    created loop for dynamic button creation and added div in html for those buttons to land inside of

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,11 +2,27 @@
 var searchword = "Ryan Gosling";
 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword + "&api_key=IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx&limit=5"
 
-$(document).on("click", function () {
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    })
-})
+// array of strings for topics
+var topics = ["history", "epidemics", "parks and recreation", "the office", "greys anatomy", "simplynailogical"];
+
+for (var i = 0; i < topics.length; i++) {
+    // need to empty div so we don't get repeat buttons on every click
+
+    // create button for each topic
+    var topicBtn = $("<button>");
+    $(topicBtn).text(topics[i]);
+    $("#button-container").append(topicBtn);
+}
+
+// $(document).on("click", function () {
+//     // $.ajax({
+//     //     url: queryURL,
+//     //     method: "GET"
+//     // }).then(function(response) {
+//     //     console.log(response);
+//     // })
+
+//     //loop to create buttons for topics array
+    
+    
+// })
