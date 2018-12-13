19321f9cd9b12d434db20dd3c25823cfba186231
Jalia Evans <irohdesign@outlook.com>
2018-12-12 20:43:47 -0500

    setting up jquery and making sure everything connects as it should

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,7 @@
 // api key = IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx
+var searchword = "Ryan Gosling";
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword + "&api_key=IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx&limit=5"
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword "&api_key=IEIzfujY4UXYx3Sd4jarElMDtOPjQNnx&limit=5"
+$(document).on("click", function () {
+    console.log("You clicked");
+})
