14015782d91f9295d2ab3aeddc86d8121ed4d9b0
Jalia Evans <irohdesign@outlook.com>
2018-12-12 21:10:08 -0500

    got the api to return still images of the gifs asked of it

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,8 +6,6 @@ var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchword + "&api_key
 var topics = ["history", "epidemics", "parks and recreation", "the office", "greys anatomy", "simplynailogical"];
 
 
-
-
 $("#btn").on("click", function () {
     event.preventDefault();
 
@@ -17,18 +15,32 @@ $("#btn").on("click", function () {
     }).then(function(response) {
         console.log(response);
 
-        var userInput = $("#testInput").val();
-        topics.push(userInput);
+        // var userInput = $("#testInput").val();
+        var results = response.data;
+        console.log(results);
 
+        // topics.push(userInput);
 
-        for (var i = 0; i < topics.length; i++) {
-            // need to empty div so we don't get repeat buttons on every click
+        // for (var i = 0; i < topics.length; i++) {
+        //     // need to empty div so we don't get repeat buttons on every click
 
-            // create button for each topic
-            var topicBtn = $("<button>");
-            $(topicBtn).text(topics[i]);
-            $("#button-container").append(topicBtn);
+        //     // create button for each topic
+        //     var topicBtn = $("<button>");
+        //     $(topicBtn).text(topics[i]);
+        //     $("#button-container").append(topicBtn);
+        // }
+
+        // grab 10 static gif images and display them
+        for (var i = 0; i < results.length; i++) {
+            var staticImgURL = results[i].images.original_still.url;
+            var imgContainer = $("<img>");
+            imgContainer.attr("src", staticImgURL);
+            $("#button-container").append(imgContainer);
+            // console.log(results[i].type);
         }
+
+        
+
     })
 
     //loop to create buttons for topics array
